5526e0fcf6d9bb15845df7ed1e9d0bcf6d2cba9f
hoogeand <hoogeand@hotmail.com>
2021-06-21 11:51:39 +1000

    End of part 3

---
+++ src/app/company/company-list/company-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Company } from '../company'
+
+@Component({
+  selector: 'fbc-company-list',
+  templateUrl: './company-list.component.html',
+  styleUrls: ['./company-list.component.scss']
+})
+export class CompanyListComponent implements OnInit {
+  companies: Company[] = [];
+
+  constructor() { }
+
+
+
+  ngOnInit(): void {
+    this.getCompanies();
+  }
+
+  getCompanies() {
+
+    this.companies = [
+      { name: 'Company 1', email: 'Email 1', phone: 1111 },
+      { name: 'Company 2', email: 'Email 2', phone: 2222 },
+      { name: 'Company 3', email: 'Email 3', phone: 3333 }
+    ]
+  }
+
+}
